ffe327b25f3fa58d4c9fed28f967dc49443f9a58
micha <info@macropage.de>
2019-05-29 09:29:05 +0200

    reindex response array to keep original sorting

--- upload_images.php
+++ upload_images.php
@@ -32,6 +32,7 @@ class upload_images {
 		$config['concurrency']     = array_key_exists('concurrency', $config) && $config['concurrency'] ? $config['concurrency'] : 10;
 		$config['comp-level']      = array_key_exists('comp-level', $config) && $config['comp-level'] ? $config['comp-level'] : 1113;
 		$config['ExtensionInDays'] = array_key_exists('ExtensionInDays', $config) && $config['ExtensionInDays'] ? $config['ExtensionInDays'] : 30;
+		$config['rewrite-index']   = array_key_exists('rewrite-index', $config) && $config['rewrite-index'] ? $config['rewrite-index'] : true;
 		$this->config              = $config;
 		$this->client              = new Client(['base_uri' => $api_uri, 'debug' => $debug]);
 	}
@@ -47,8 +48,8 @@ class upload_images {
 		$client   = $this->client;
 		$config   = $this->config;
 		$requests = static function ($images) use ($client, $config) {
-			foreach ($images as $imageData) {
-				yield static function () use ($client, $imageData, $config) {
+			foreach ($images as $index => $imageData) {
+				yield static function () use ($client, $imageData, $config, $index) {
 					return $client->postAsync('/ws/api.dll', [
 						'headers'   => [
 							'X-EBAY-API-APP-NAME'            => $config['app-name'],
@@ -66,7 +67,7 @@ class upload_images {
     <RequesterCredentials>
         <ebl:eBayAuthToken xmlns:ebl="urn:ebay:apis:eBLBaseComponents">' . $config['auth-token'] . '</ebl:eBayAuthToken>
     </RequesterCredentials>
-    <PictureName>something</PictureName>
+    <PictureName>'.$index.'</PictureName>
     <PictureSet>Standard</PictureSet>
     <ExtensionInDays>' . $config['ExtensionInDays'] . '</ExtensionInDays>
 </UploadSiteHostedPicturesRequest>',
@@ -121,7 +122,59 @@ class upload_images {
 			$responses_parsed[$index] = $response['parsed_body']['SiteHostedPictureDetails'];
 		}
 
-		return ['state' => true, 'responses' => $responses_parsed];
+		return ['state' => true, 'responses' => $this->rewriteIndex($responses_parsed)];
 	}
+
+    /**
+     * in case you use an image of arrays with a random numbering, like
+     *
+     * $images = [
+     *     1 => ....img-data....,
+     *     5 => ....img-data....,
+     *     8 => ....img-data....,
+     *     4 => ....img-data....,
+     * ]
+     *
+     * you get the same keys back in your global response array
+     * in that case you are able to match the ebay result to your
+     * image-data
+     *
+     * you will get:
+     *
+     * $responses = [
+     *    1 => response,
+     *    4 => response,
+     *    5 => response,
+     *    8 => response,
+     * ]
+     *
+     * instead of
+     *
+     * $responses = [
+     *    0 => response,
+     *    1 => response,
+     *    2 => response,
+     *    3 => response,
+     * ]
+     *
+     * why ? because we asume that you want a special order of your images in your ebay item ;)
+     * you should be able to use the same order of results for your "PictureURL" in your reviseitem/additem
+     * as you used here for the uploads
+     *
+     * @param array $responses
+     *
+     * @return array
+     */
+	private function rewriteIndex(array $responses) {
+	    if ($this->config['rewrite-index']) {
+	        $new_array = [];
+	        foreach ($responses as $UploadResponse) {
+	            $new_array[(int)$UploadResponse['PictureName']] = $UploadResponse;
+            }
+	        ksort($new_array,SORT_NUMERIC);
+	        return $new_array;
+        }
+	    return $responses;
+    }
 
 }
